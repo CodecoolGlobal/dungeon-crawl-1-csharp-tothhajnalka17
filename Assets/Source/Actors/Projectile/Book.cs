398bbe6b40dbc2cf12a00f8d25aa49439590cd0c
robert <rnagy89@gmail.com>
2022-09-21 13:52:18 +0200

    Feat: Add Book class under Projectile namespace

---
+++ Assets/Source/Actors/Projectile/Book.cs
@@ -0,0 +1,53 @@
+﻿using Assets.Source.Core;
+using DungeonCrawl.Actors.Characters;
+using DungeonCrawl.Actors;
+using DungeonCrawl.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DungeonCrawl.Actors.Static;
+using DungeonCrawl.Actors.Items;
+
+namespace Assets.Source.Actors.Projectile
+{
+    public class Book : Actor
+    {
+        public int Damage;
+        public override int DefaultSpriteId => 559;
+        public override string DefaultName => "Key";
+        public override bool Detectable => true;
+
+        public Book()
+        {
+            Damage = 10;
+        }
+        public override bool OnCollision(Actor anotherActor)
+        {
+            if (anotherActor is Player)
+            {
+                Player player = (Player)anotherActor;
+                player.ApplyDamage(Damage);
+                ActorManager.Singleton.DestroyActor(this);
+            }
+            else if (anotherActor is Skeleton)
+            {
+                Skeleton skeleton = (Skeleton)anotherActor;
+                skeleton.ApplyDamage(Damage);
+                ActorManager.Singleton.DestroyActor(this);
+            }
+            else if (anotherActor is Wall || anotherActor is Item)
+            {
+                ActorManager.Singleton.DestroyActor(this);
+            }
+
+            return false;
+        }
+
+        protected override void OnUpdate(float deltatime)
+        {
+
+        }
+    }
+}
