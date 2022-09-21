f9bc768410b3e40b50bc7725da375b17638f8ba2
robert <rnagy89@gmail.com>
2022-09-21 20:17:17 +0200

    Feat: Add Projectiles abstract class // now all spells inherit from Projectile class

--- Assets/Source/Actors/Projectile/Obliviate.cs
+++ Assets/Source/Actors/Projectile/Obliviate.cs
@@ -1,5 +1,4 @@
-﻿using Assets.Source.Core;
-using DungeonCrawl.Actors.Characters;
+﻿using DungeonCrawl.Actors.Characters;
 using DungeonCrawl.Actors;
 using DungeonCrawl.Core;
 using System;
@@ -7,19 +6,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using DungeonCrawl.Actors.Static;
-using DungeonCrawl.Actors.Items;
 using DungeonCrawl;
 using UnityEngine;
 
 namespace Assets.Source.Actors.Projectile
 {
-    public class Flipendo : Projectile
+    public class Obliviate : Projectile
     {
-        public override int DefaultSpriteId => 666;
-        public override string DefaultName => "Flipendo";
+        public override int DefaultSpriteId => 442;
+        public override string DefaultName => "Obliviate";
         public override bool Detectable => true;
-        public Flipendo()
+        public Obliviate()
         {
             Direction = DefaultDirection;
             LifeTime = 0;
@@ -27,7 +24,6 @@ namespace Assets.Source.Actors.Projectile
         }
         public override bool OnCollision(Actor anotherActor)
         {
-            Debug.Log($"Collisiong with {anotherActor}");
             if (anotherActor is Skeleton)
             {
                 var enemy = (Skeleton)anotherActor;
@@ -35,25 +31,19 @@ namespace Assets.Source.Actors.Projectile
             }
             if (anotherActor.OnCollision(this))
             {
-                return false;
+                return true;
             }
             else
             {
                 ActorManager.Singleton._allActors.Remove(this);
                 ActorManager.Singleton.DestroyActor(this);
-                return false;
+                return true;
             }
-            
+
         }
 
         protected override void OnUpdate(float deltatime)
         {
-            LifeTime++;
-            if (LifeTime > 24)
-            {
-                TryMove(Direction);
-                LifeTime = 0;
-            }
         }
     }
 }
