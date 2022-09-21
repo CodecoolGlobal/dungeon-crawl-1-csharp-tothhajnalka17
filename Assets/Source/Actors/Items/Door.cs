2049e5b7077624f402bd15cda803301675843525
robert <rnagy89@gmail.com>
2022-09-21 12:05:41 +0200

    Refactor: Add Items namespace // now Items are subclass of Item which is a subclass of Actor

---
+++ Assets/Source/Actors/Items/Door.cs
@@ -0,0 +1,38 @@
+﻿using DungeonCrawl.Actors.Characters;
+using DungeonCrawl.Actors.Items;
+using DungeonCrawl.Actors;
+using DungeonCrawl.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Source.Actors.Items;
+
+namespace Assets.Source.Actors.Static
+{
+    public class Door : Item
+    {
+        public override int DefaultSpriteId => 146;
+        public override string DefaultName => "Door";
+        public override bool Detectable => true;
+        public override bool OnCollision(Actor anotherActor)
+        {
+            if (anotherActor is Player)
+            {
+                Player player = (Player)anotherActor;
+                foreach (var item in player.Inventory)
+                {
+                    if (item is Key)
+                    {
+                        player.Inventory.Remove(item);
+                        ActorManager.Singleton.DestroyActor(this);
+                        ActorManager.Singleton.Spawn<OpenDoor>(this.Position);
+                        break;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Assets/Source/Actors/Items/Wand.cs
@@ -0,0 +1,30 @@
+﻿using DungeonCrawl.Actors.Characters;
+using DungeonCrawl.Actors;
+using DungeonCrawl.Core;
+using System.Collections;
+using UnityEngine;
+
+namespace Assets.Source.Actors.Static
+{
+    public class Wand : Actor
+    {
+        public override int DefaultSpriteId => 178;
+        public override string DefaultName => "Wand";
+        public override bool Detectable => true;
+        public override bool IsItem()
+        {
+            return true;
+        }
+
+        public override bool OnCollision(Actor anotherActor)
+        {
+            if (anotherActor is Player)
+            {
+                Player player = (Player)anotherActor;
+                player.AddToInvetory(this);
+                ActorManager.Singleton.DestroyActor(this);
+            }
+            return false;
+        }
+    }
+}
